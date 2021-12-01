9772e6cb50317a7666f1d749765c6a7e0f5db09f
Sergio Casero Hernández <sergio.casero23@gmail.com>
2021-12-01 12:26:51 +0100

    Using kotlin-dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,107 +4,30 @@ plugins {
     id("com.gradle.plugin-publish") version "0.16.0"
 }
 
-abstract class KmmModuleGeneratorTask : DefaultTask() {
+version = "1.0"
 
-    @get:Input
-    @set:Option(option = "moduleName", description = "The name of the module you want to create")
-    var moduleName: String = ""
-
-    @get:Input
-    @set:Option(option = "modulePath", description = "The root path for the new module")
-    var modulePath: String = ""
-
-    @get:Input
-    @Option(option = "packageName", description = "The packageName")
-    var packageName: String = ""
-
-
-    private fun fromPackageNameToPath(packageName: String) =
-        packageName.replace(".", "/")
-
-    private fun createManifestFile(packageName: String) = """
-            <?xml version="1.0" encoding="utf-8"?>
-            <manifest package="$packageName" />
-        """.trimIndent()
-
-    private fun createBuildGradleKtsFile(name: String) = """
-            import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-            plugins {
-                kotlin("multiplatform")
-                kotlin("native.cocoapods")
-                id("com.android.library")
-            }
-            version = "1.0"
-            kotlin {
-                android()
-                val iosTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget = when {
-                    System.getenv("SDK_NAME")?.startsWith("iphoneos") == true -> ::iosArm64
-                    System.getenv("NATIVE_ARCH")?.startsWith("arm") == true -> ::iosSimulatorArm64
-                    else -> ::iosX64
-                }
-                iosTarget("ios") {}
-                cocoapods {
-                    summary = TODO("Define the module Summary")
-                    homepage = TODO("Define the module homepage")
-                    ios.deploymentTarget = TODO("Define the module homepage")
-                    framework {
-                        baseName = "$name"
-                        isStatic = false
-                        linkerOpts.add("-lsqlite3")
-                    }
-                    podfile = project.file(TODO("Podifile Path"))
-                }
-                sourceSets {
-                    val commonMain by getting { dependencies {} }
-                    val commonTest by getting { dependencies {} }
-                    val androidMain by getting { dependencies {} }
-                    val androidTest by getting { dependencies {} }
-                    val iosMain by getting { dependencies {} }
-                    val iosTest by getting { dependencies {} }
-                }
-            }
-            android {
-                compileSdkVersion(31)
-                sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-                defaultConfig {
-                    minSdkVersion(23)
-                    targetSdkVersion(31)
-                }
-            }
-
-        """.trimIndent()
-
-    private fun sourceSet(basePath: String, variant: String) =
-        "$basePath/src/$variant/kotlin/${fromPackageNameToPath(packageName)}/$moduleName"
-
-    @TaskAction
-    fun doWork() {
-        val basePath = "${project.projectDir}/$modulePath/$moduleName/"
-        println("Creating module $moduleName")
-        println("Creating commonMain...")
-        project.mkdir(File(sourceSet(basePath, "commonMain")))
-        println("Creating androidMain...")
-        project.mkdir(File(sourceSet(basePath, "androidMain")))
-        println("Creating iosMain...")
-        project.mkdir(File(sourceSet(basePath, "iosMain")))
-
-        println("Creating AndroidManifest...")
-        val manifest = File("$basePath/src/androidMain/AndroidManifest.xml")
-        manifest.createNewFile()
-        manifest.writeText(createManifestFile(packageName))
-
-        println("Creating build.gradle.kts file...")
-        val gradleKts = File("$basePath/build.gradle.kts")
-        gradleKts.createNewFile()
-        gradleKts.writeText(createBuildGradleKtsFile(moduleName))
-
-        println("Everything is done!")
-    }
-}
-
-open class KmmModuleGeneratorPlugin : Plugin<Project> {
-    override fun apply(target: Project) {
-        target.tasks.register<KmmModuleGeneratorTask>("createKmmModule")
+gradlePlugin {
+    plugins {
+        create("KmmModuleGenerator") {
+            id = "com.sergiocasero.generator"
+            displayName = "Kotlin Multiplatform Module Generator"
+            description = """
+                This plugin ables you to create so fast a new kotlin multiplatform plugin with all the source sets,
+                Example usage:
+                
+                ./gradlew -q createKmmModule --moduleName=poiui --modulePath=shared/poi --packageName=com.sergiocasero.poi.ui
+                
+                This command will generate these folders and files:
+                shared/poi/poiui/src/commonMain/kotlin/com/sergiocasero/poi/ui/poiui
+                shared/poi/poiui/src/iosMain/kotlin/com/sergiocasero/poi/ui/poiui
+                shared/poi/poiui/src/androidMain/kotlin/com/sergiocasero/poi/ui/poiui
+                shared/poi/poiui/src/androidMain/AndroidManifest.xml
+                shared/poi/poiui/build.gradle.kts
+                
+                And that's it :)
+                
+            """.trimIndent()
+        }
     }
 }
 
